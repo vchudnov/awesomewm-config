13d0f0bcd6d99500b9d32cdc77067fc02bac4409
Victor Chudnovsky <victor.chudnovsky+github@gmail.com>
2018-12-26 16:10:18 -0800

    Change a number of keyboard shortcuts

--- rc.lua
+++ rc.lua
@@ -247,11 +247,11 @@ root.buttons(gears.table.join(
 
 -- {{{ Key bindings
 globalkeys = gears.table.join(
-    awful.key({ modkey,           }, "s",      hotkeys_popup.show_help,
+    awful.key({ modkey,           }, "F1",      hotkeys_popup.show_help,
               {description="show help", group="awesome"}),
-    awful.key({ modkey,           }, "Left",   awful.tag.viewprev,
+    awful.key({ modkey, "Control" }, "Left",   awful.tag.viewprev,
               {description = "view previous", group = "tag"}),
-    awful.key({ modkey,           }, "Right",  awful.tag.viewnext,
+    awful.key({ modkey, "Control" }, "Right",  awful.tag.viewnext,
               {description = "view next", group = "tag"}),
     awful.key({ modkey,           }, "Escape", awful.tag.history.restore,
               {description = "go back", group = "tag"}),
@@ -306,28 +306,32 @@ globalkeys = gears.table.join(
     -- TODO: modify the above to change all screens at the same time!
     
     -- Standard program
-    awful.key({ modkey,           }, "Return", function () awful.spawn(terminal) end,
+    awful.key({ modkey, "Control" }, "Return", function () awful.spawn(terminal) end,
               {description = "open a terminal", group = "launcher"}),
-    awful.key({ modkey, "Control" }, "r", awesome.restart,
+    awful.key({ modkey, "Control" }, "F5", awesome.restart,
               {description = "reload awesome", group = "awesome"}),
-    awful.key({ modkey, "Shift"   }, "q", awesome.quit,
+    awful.key({ modkey, "Control", "Alt", "Shift"   }, "Escape", awesome.quit,
               {description = "quit awesome", group = "awesome"}),
 
-    awful.key({ modkey,           }, "l",     function () awful.tag.incmwfact( 0.05)          end,
-              {description = "increase master width factor", group = "layout"}),
-    awful.key({ modkey,           }, "h",     function () awful.tag.incmwfact(-0.05)          end,
-              {description = "decrease master width factor", group = "layout"}),
-    awful.key({ modkey, "Shift"   }, "h",     function () awful.tag.incnmaster( 1, nil, true) end,
-              {description = "increase the number of master clients", group = "layout"}),
-    awful.key({ modkey, "Shift"   }, "l",     function () awful.tag.incnmaster(-1, nil, true) end,
-              {description = "decrease the number of master clients", group = "layout"}),
-    awful.key({ modkey, "Control" }, "h",     function () awful.tag.incncol( 1, nil, true)    end,
+    awful.key({ modkey      }, "equal",     function () awful.tag.incmwfact( 0.05)          end,
+       {description = "increase master width factor", group = "layout"}),
+    
+    awful.key({ modkey,           }, "minus",     function () awful.tag.incmwfact(-0.05)          end,
+       {description = "decrease master width factor", group = "layout"}),
+    
+    awful.key({ modkey, "Control"   }, "equal",     function () awful.tag.incnmaster( 1, nil, true) end,
+       {description = "increase the number of master clients", group = "layout"}),
+    awful.key({ modkey, "Control"   }, "minus",     function () awful.tag.incnmaster(-1, nil, true) end,
+       {description = "decrease the number of master clients", group = "layout"}),
+    
+    awful.key({ modkey, "Control", "Shift" }, "equal",     function () awful.tag.incncol( 1, nil, true)    end,
               {description = "increase the number of columns", group = "layout"}),
-    awful.key({ modkey, "Control" }, "l",     function () awful.tag.incncol(-1, nil, true)    end,
-              {description = "decrease the number of columns", group = "layout"}),
-    awful.key({ modkey,           }, "space", function () awful.layout.inc( 1)                end,
+    awful.key({ modkey, "Control", "Shift" }, "minus",     function () awful.tag.incncol(-1, nil, true)    end,
+       {description = "decrease the number of columns", group = "layout"}),
+    
+    awful.key({ modkey }, "Next", function () awful.layout.inc( 1)                end,
               {description = "select next", group = "layout"}),
-    awful.key({ modkey, "Shift"   }, "space", function () awful.layout.inc(-1)                end,
+    awful.key({ modkey }, "Prior", function () awful.layout.inc(-1)                end,
               {description = "select previous", group = "layout"}),
 
     awful.key({ modkey, "Control" }, "n",
@@ -342,7 +346,7 @@ globalkeys = gears.table.join(
               {description = "restore minimized", group = "client"}),
 
     -- Prompt
-    awful.key({ modkey },            "r",     function () awful.screen.focused().mypromptbox:run() end,
+    awful.key({ modkey },            "Return",     function () awful.screen.focused().mypromptbox:run() end,
               {description = "run prompt", group = "launcher"}),
 
     awful.key({ modkey }, "x",
@@ -356,12 +360,12 @@ globalkeys = gears.table.join(
               end,
               {description = "lua execute prompt", group = "awesome"}),
     -- Menubar
-    awful.key({ modkey }, "p", function() menubar.show() end,
+    awful.key({ modkey }, "F4", function() menubar.show() end,
        {description = "show the menubar", group = "launcher"}),
 
     -- Move all screens to previus/next tag
     -- https://superuser.com/questions/556877/simultaneously-switch-tags-as-one-screen-in-multi-monitor-setup
-    awful.key({ modkey, "Control"   }, "Left", 
+    awful.key({ modkey }, "Left", 
        function()
 	  for i = 1, screen.count() do
 	     awful.tag.viewprev(screen[i])
@@ -369,7 +373,7 @@ globalkeys = gears.table.join(
        end,
        {description="view previous on all screens", group = "tag"}),
     
-    awful.key({ modkey, "Control"   }, "Right", 
+    awful.key({ modkey }, "Right", 
        function()
 	  for i = 1, screen.count() do
 	     awful.tag.viewnext(screen[i])
@@ -379,13 +383,13 @@ globalkeys = gears.table.join(
     )
 
 clientkeys = gears.table.join(
-    awful.key({ modkey,           }, "f",
+    awful.key({ modkey }, "F11",
         function (c)
             c.fullscreen = not c.fullscreen
             c:raise()
         end,
         {description = "toggle fullscreen", group = "client"}),
-    awful.key({ modkey, "Shift"   }, "c",      function (c) c:kill()                         end,
+    awful.key({ modkey, "Control", "Alt"   }, "Escape",      function (c) c:kill()                         end,
               {description = "close", group = "client"}),
     awful.key({ modkey, "Control" }, "space",  awful.client.floating.toggle                     ,
               {description = "toggle floating", group = "client"}),
@@ -394,27 +398,31 @@ clientkeys = gears.table.join(
     awful.key({ modkey,           }, "o",      function (c) c:move_to_screen()               end,
               {description = "move to screen", group = "client"}),
     awful.key({ modkey,           }, "t",      function (c) c.ontop = not c.ontop            end,
-              {description = "toggle keep on top", group = "client"}),
-    awful.key({ modkey,           }, "n",
+       {description = "toggle keep on top", group = "client"}),
+    
+    awful.key({ modkey, "Control" }, "F9",
         function (c)
             -- The client currently has the input focus, so it cannot be
             -- minimized, since minimized clients can't have the focus.
             c.minimized = true
         end ,
         {description = "minimize", group = "client"}),
-    awful.key({ modkey,           }, "m",
+    
+    awful.key({ modkey }, "F9",
         function (c)
             c.maximized = not c.maximized
             c:raise()
         end ,
         {description = "(un)maximize", group = "client"}),
-    awful.key({ modkey, "Control" }, "m",
+    
+    awful.key({ modkey }, "F10",
         function (c)
             c.maximized_vertical = not c.maximized_vertical
             c:raise()
         end ,
         {description = "(un)maximize vertically", group = "client"}),
-    awful.key({ modkey, "Shift"   }, "m",
+    
+    awful.key({ modkey }, "F12",
         function (c)
             c.maximized_horizontal = not c.maximized_horizontal
             c:raise()
